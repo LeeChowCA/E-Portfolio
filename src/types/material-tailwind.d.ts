7817d04ede19873dca11eb28ceb908e6f6d19a85
Lee <zhoujianpingls@outlook.com>
2025-04-28 22:53:05 -0700

    ""

--- src/types/material-tailwind.d.ts
+++ src/types/material-tailwind.d.ts
@@ -1,14 +1,69 @@
 declare module "@material-tailwind/react" {
     import * as React from "react";
+
+    // Typography component
     export interface TypographyProps extends React.HTMLAttributes<HTMLElement> {
         variant?: string;
         color?: string;
         className?: string;
         children?: React.ReactNode;
-        placeholder?: string;
-        onPointerEnterCapture?: React.PointerEventHandler<HTMLElement>;
-        onPointerLeaveCapture?: React.PointerEventHandler<HTMLElement>;
-        // Add any other props you use
     }
     export const Typography: React.FC<TypographyProps>;
+
+    // Card components
+    export interface CardProps extends React.HTMLAttributes<HTMLDivElement> {
+        shadow?: boolean;
+        className?: string;
+        color?: string;
+    }
+    export const Card: React.FC<CardProps>;
+
+    export interface CardBodyProps extends React.HTMLAttributes<HTMLDivElement> {
+        className?: string;
+    }
+    export const CardBody: React.FC<CardBodyProps>;
+
+    // Form components
+    export interface RadioProps {
+        color?: string;
+        label?: React.ReactNode;
+        name?: string;
+        defaultChecked?: boolean;
+    }
+    export const Radio: React.FC<RadioProps>;
+
+    export interface InputProps {
+        color?: string;
+        size?: string;
+        variant?: string;
+        label?: string;
+        name?: string;
+        placeholder?: string;
+        containerProps?: any;
+    }
+    export const Input: React.FC<InputProps>;
+
+    export interface TextareaProps {
+        color?: string;
+        size?: string;
+        variant?: string;
+        label?: string;
+        name?: string;
+        containerProps?: any;
+    }
+    export const Textarea: React.FC<TextareaProps>;
+
+    export interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+        color?: string;
+        size?: string;
+        variant?: string;
+        className?: string;
+    }
+    export const Button: React.FC<ButtonProps>;
+
+    export interface IconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+        color?: string;
+        variant?: string;
+    }
+    export const IconButton: React.FC<IconButtonProps>;
 }
