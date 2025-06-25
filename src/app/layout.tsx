dc32a15198f5fdc6095070567be80b1e651272f3
Lee <zhoujianpingls@outlook.com>
2025-06-25 15:59:09 -0700

    moved Analytics component out of Layout, but before body

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -35,9 +35,9 @@ export default function RootLayout({
       <body className={roboto.className}>
         <Layout>
           {children}
-          <Analytics />
           <FixedPlugin />
         </Layout>
+        <Analytics />
       </body>
     </html>
   );
